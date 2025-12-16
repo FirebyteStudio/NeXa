d4e97a7fed5307f4764956a6072f017c2808dacb
Firebyte Studio <firebyte_studio@outlook.com>
2025-12-16 12:48:30 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+
+export const supabase = createClient(
+'https://lhpksscyvlxjsmrrghsj.supabase.co',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxocGtzc2N5dmx4anNtcnJnaHNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4OTQzNjUsImV4cCI6MjA4MTQ3MDM2NX0.wxYmDIi56vXV8gxUVbhVK84uQdpjY6bbXMrqfC2XRRA'
+)
